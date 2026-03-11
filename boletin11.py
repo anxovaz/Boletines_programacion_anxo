6b14a7be8a6f9c6fa695cb6c51c762b51be66125
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-03-11 13:29:18 +0100

    boletin11 ej2

--- boletin11.py
+++ boletin11.py
@@ -15,9 +15,57 @@ O usuario pode buscar notas que conteñan unha palabra clave.
 '''
 
 def ejercicio1():
+    print("Ejercicio 1")
     with open('./archivos_boletin11/ejercicio1.txt',"w") as f:
         inputUsuario = str(input("Ingrese su nota: "))
         f.write(inputUsuario)
         f.close()
 
 ejercicio1()
+
+'''
+Ler un ficheiro de texto e contar cantas veces aparece cada palabra.
+Solicita ao usuario o nome dun ficheiro .txt.
+
+
+Mostra a frecuencia de cada palabra (ignorando maiúsculas/minúsculas e signos de puntuación).
+
+
+Gárdase un resumo nun novo ficheiro resumo_palabras.txt.
+
+
+'''
+
+def ejercicio2():
+    print("Ejercicio2")
+    #fich = str(input("Introduce ruta del archivo: "))
+    fich = "./archivos_boletin11/ejercicio2.txt"
+    splitFich = []
+    with open(fich,"r") as f:
+        for i in f.readlines():
+            for j in i.split(" "):
+                splitFich.append(j)
+
+
+    palabras = []
+    for i in splitFich:
+        if ejercicio2_comprobar_palabra(i,palabras):
+            input = []
+            input.append(i)
+            input.append(splitFich.count(i))
+            palabras.append(input)
+
+
+
+    return palabras
+
+
+def ejercicio2_comprobar_palabra(palabra, lista):
+    for i in lista:
+        for j in i:
+            if palabra == j:
+                return False
+            break #solo me interesa la primera posición que es en la que va el nombre del elemento
+    return True
+
+print(f"Ejercicio2: {ejercicio2()}")
