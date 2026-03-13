d2d06a281f322d1deaed40c87969115e09d25d01
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-03-13 10:54:13 +0100

    boletin11 clase Cliente

--- boletin11.py
+++ boletin11.py
@@ -144,8 +144,61 @@ def ejercicio3():
                 print("  ---  ")
                 print(i)
 
-
-
-
-
 ejercicio3()
+
+print("---------------------Ejercicio 4------------------")
+def ejercicio4():
+    class Cliente:
+        def __init__(self,id, nombre, telefono):
+            self.id = id
+            self.nombre = nombre
+            self.telefono = telefono
+
+        @property
+        def id(self):
+            return self.__id
+        @property
+        def nombre(self):
+            return self.__nombre
+        @property
+        def telefono(self):
+            return self.__telefono
+        @id.setter
+        def id(self,id):
+            if isinstance(id, str):
+                if len(id) == 4:
+                    self.__id = id
+                else:
+                    raise ValueError
+            else:
+                raise TypeError
+        @nombre.setter
+        def nombre(self,nombre):
+            if isinstance(nombre, str):
+                self.__nombre = nombre
+            else:
+                raise TypeError
+        @telefono.setter
+        def telefono(self,telefono):
+            if isinstance(telefono, str):
+                if telefono.isdigit() and len(telefono) == 9:
+                    self.__telefono = telefono
+                else:
+                    raise ValueError
+            elif isinstance(telefono, int):
+                telefono = str(telefono)
+                if len(telefono) == 9:
+                    self.__telefono = telefono
+                else:
+                    raise ValueError
+            elif isinstance(telefono, float):
+                telefono = str(int(telefono))
+                if len(telefono) == 9:
+                    self.__telefono = telefono
+                else:
+                    raise ValueError
+            else:
+                raise TypeError
+
+        def __str__(self):
+            return f"Nombre:{self.__nombre}\nTelefono:{self.__telefono}\nid:{self.__id}"
